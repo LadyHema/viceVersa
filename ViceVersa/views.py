10e8d3d3cdf4129f3fa7a0b3c2c8b31e010443e3
user684 <njuggel@gmail.com>
2020-10-09 15:42:35 +0300

    add reverse and count words in text functional

--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# def about(request):
-#     return HttpResponse('Answer about page')
-
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reverse_text = user_text[::-1]
+    count_words = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext': user_text,
+                                            'reversetext': reverse_text,
+                                            'cnt': count_words})
